1a60c14674a6097744ecac5e27ee6216568ca6c5
Lautaro <kautarol@gmail.com>
2024-05-09 18:50:07 -0300

    Improving Home Styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/NavBar.css";
 import house from '../images/houseIMG1.png';
 import world from '../images/world0.png';
 import { Link } from "react-router-dom";
 
 export default function NavBar(){
+
+    const [lengMenu, setLengMenu] = useState(false);
+
+    function handleLengMenu(e){
+        e.preventDefault();
+        setLengMenu(!lengMenu);
+    }
 
     return(
        <div className="navBarContent">
@@ -23,8 +30,17 @@ export default function NavBar(){
                         <button className="menuBtn">Sobre mÍ</button>
                     </Link>
                 </div>
-                    <img className="lenguageBtn" alt="botonDeIdioma" src={world}/>
+                    <img className="lenguageBtn" alt="botonDeIdioma" onClick={handleLengMenu} src={world}/>
             </div>  
+            {
+                lengMenu ? 
+                <div className="lengMenu">
+                    <button className="menuBtn">Español</button>
+                    <button className="menuBtn">English</button>
+                </div>
+                :
+                null
+            }
        </div> 
     )
 }
